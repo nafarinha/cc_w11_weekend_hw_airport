8e1cc455bb1000c553462ac30583ff356b4c8d36
Nuno Farinha <nadfarinha@gmail.com>
2019-03-29 18:21:29 +0000

    Create get flight number function on ticket class

--- src/main/java/Ticket.java
+++ src/main/java/Ticket.java
@@ -1,2 +1,15 @@
 public class Ticket {
+
+    private int ticketNumber;
+    private String origin;
+    private String destination;
+    private String flightNumber;
+
+    public Ticket(String flightNumber) {
+        this.flightNumber = flightNumber;
+    }
+
+    public String getFlightNumber() {
+        return this.flightNumber;
+    }
 }
--- src/test/java/TicketTest.java
+++ src/test/java/TicketTest.java
@@ -1,2 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class TicketTest {
+
+    Ticket ticket1;
+
+    @Before
+    public void before() {
+        ticket1 = new Ticket("DY3503");
+    }
+
+    @Test
+    public void hasFlightNumber() {
+        assertEquals("DY3503", ticket1.getFlightNumber());
+    }
 }
