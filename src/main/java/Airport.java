ecdb02e87f0eb66cccf19c2cc92a2e935c17c4cd
Nuno Farinha <nadfarinha@gmail.com>
2019-03-31 20:36:58 +0100

    Create get code function on airport class

--- src/main/java/Airport.java
+++ src/main/java/Airport.java
@@ -1,2 +1,17 @@
+import java.util.ArrayList;
+
 public class Airport {
+
+    private String code;
+    private ArrayList<Plane> hangar;
+
+    public Airport(String code) {
+        this.code = code;
+        this.hangar = new ArrayList<>();
+
+    }
+
+    public String getCode() {
+        return this.code;
+    }
 }
--- src/test/java/AirportTest.java
+++ src/test/java/AirportTest.java
@@ -1,2 +1,25 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class AirportTest {
+    Ticket ticket1;
+    Plane plane1, plane2;
+    Flight flight1;
+    Airport airport1;
+
+    @Before
+    public void before() {
+        ticket1 = new Ticket("DY3503");
+        plane1 = new Plane(PlaneType.BOEING777, "Norwegian");
+        plane2 = new Plane(PlaneType.AIRBUSA330, "KLM");
+        flight1 = new Flight("DY3503", "AMS", plane1);
+        airport1 = new Airport("EDI");
+    }
+
+    @Test
+    public void canGetCode() {
+        assertEquals("EDI", airport1.getCode());
+    }
 }
