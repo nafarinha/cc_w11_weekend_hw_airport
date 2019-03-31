f22603ec5d5c4ce748c45676b5b55218d316aeec
Nuno Farinha <nadfarinha@gmail.com>
2019-03-31 19:45:09 +0100

    Add getter methods for properties on plane class

--- src/main/java/Plane.java
+++ src/main/java/Plane.java
@@ -1,2 +1,26 @@
+import java.util.ArrayList;
+
 public class Plane {
+
+    private PlaneType type;
+    private String airline;
+    private ArrayList<Passenger> passengers;
+
+    public Plane(PlaneType type, String airline) {
+        this.type = type;
+        this.airline = airline;
+        passengers = new ArrayList<>();
+    }
+
+    public PlaneType getType() {
+        return this.type;
+    }
+
+    public String getAirline() {
+        return this.airline;
+    }
+
+    public ArrayList<Passenger> getPassengers() {
+        return this.passengers;
+    }
 }
--- src/test/java/PlaneTest.java
+++ src/test/java/PlaneTest.java
@@ -1,2 +1,35 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PlaneTest {
+
+    Plane plane1;
+    Passenger passenger1, passenger2, passenger3;
+
+    @Before
+    public void before() {
+        plane1 = new Plane(PlaneType.AIRBUSA380, "ScotAir");
+
+        passenger1 = new Passenger("Smith", "Samantha");
+        passenger2 = new Passenger("Doe", "Jonathan");
+        passenger3 = new Passenger("Davids", "Keith");
+    }
+
+    @Test
+    public void canGetPlaneType() {
+        assertEquals(PlaneType.AIRBUSA380, plane1.getType());
+    }
+
+    @Test
+    public void canGetAirline() {
+        assertEquals("ScotAir", plane1.getAirline());
+    }
+
+    @Test
+    public void canGetPassengers() {
+        assertEquals(0, plane1.getPassengers().size());
+    }
+
 }
