aa8558afc363bc13c2fbf0f24dba42adb27fbab8
Nuno Farinha <nadfarinha@gmail.com>
2019-03-31 20:07:14 +0100

    Create get flight number function on flight class

--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -1,2 +1,16 @@
 public class Flight {
+
+    private String flightNumber;
+    private String destination;
+    private Plane plane;
+
+    public Flight(String flightNumber, String destination, Plane plane) {
+        this.flightNumber = flightNumber;
+        this.destination = destination;
+        this.plane = plane;
+    }
+
+    public String getFlightNumber() {
+        return this.flightNumber;
+    }
 }
--- src/test/java/FlightTest.java
+++ src/test/java/FlightTest.java
@@ -1,2 +1,22 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class FlightTest {
+
+    Plane plane1;
+    Flight flight1;
+
+
+    @Before
+    public void before() {
+        plane1 = new Plane(PlaneType.AIRBUSA330, "Norwegian");
+        flight1 = new Flight("DY3505", "CPH", plane1);
+    }
+
+    @Test
+    public void canGetFlightNumber() {
+        assertEquals("DY3505", flight1.getFlightNumber());
+    }
 }
